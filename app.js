390a77292fef6e312081c00d4387a144b9b40275
Zakari Adamu <ojonimizakari@gmail.com>
2024-02-14 20:58:06 +0100

    wip localStorage set

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //variables
-
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
@@ -9,34 +8,70 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const ProductsDOM = document.querySelector(".products-center");
-
 // cart
 let cart = [];
-
 // getting the products
 class Products {
-	async getProducts()    {
+	async getProducts() {
 		try {
 			let result = await fetch("products.json");
 			let data = await result.json();
-            let products = data.items;
-            products = products.map(item => {
-                const { title, price } = item.fields;
-				const { image } = image;
-            })
+			let products = data.items;
+			products = products.map((item) => {
+				const { title, price } = item.fields;
+				const { id } = item.sys;
+				const image = item.fields.image.fields.file.url;
+				return { title, price, id, image };
+			});
+			return products;
 		} catch (error) {
 			console.log(error);
 		}
 	}
 }
-
 // display products
-class UI {}
-
+class UI {
+	displayProducts(products) {
+		let result = "";
+		products.forEach((product) => {
+			result += `
+			<!-- single product -->
+				<article class="product">
+					<div class="img-container">
+						<img
+							src=${product.image}
+							alt="product"
+							class="product-img"
+							srcset=""
+						/>
+						<button class="bag-btn" data-id=${product.id}>
+							<i class="fas fa-shopping-cart"></i>
+							add to cart
+						</button>
+					</div>
+					<h3>${product.title}</h3>
+					<h4>$${product.price}</h4>
+				</article>
+				<!-- end of single product
+			`;
+		});
+		ProductsDOM.innerHTML = result;
+	}
+}
 // local storage
-class Storage {}
+class Storage {
+	static saveProducts(products) {
+		localStorage.setItem("products", JSON.stringify(products));
+	}
+}
 
 document.addEventListener("DOMContentLoaded", () => {
 	const ui = new UI();
 	const products = new Products();
+
+	// get all products
+	products.getProducts().then((products) => {
+		ui.displayProducts(products);
+		Storage.saveProducts(products);
+	});
 });
